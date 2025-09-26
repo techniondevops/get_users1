34c7a9ea8a7c7e0e0f6f7595b4c80372f185ef4f
Eli Dror <eli.d@technion.ac.il>
2025-09-26 12:58:18 +0300

    lastname

--- app/users.py
+++ app/users.py
@@ -8,7 +8,7 @@ def get_users() -> list:
 
 def print_users(users:list) -> None:
     for user in users:
-        print (user['firstName'])
+        print (user['firstName'],user[lastnaME])
 
 
 def main():
